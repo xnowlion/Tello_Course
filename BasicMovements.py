07d03baa7dd1257b1c286aa1d8d87fa1bfa4c4d6
snowlion <xnowlion@gmail.com>
2025-09-10 19:48:13 -0700

    Successful connection with drone. Adding a backward flip.

--- BasicMovements.py
+++ BasicMovements.py
@@ -10,11 +10,19 @@ print(me.get_battery())
 
 me.takeoff()
 
+me.send_rc_control(0, 0, 0, 0)
+
+sleep(7)
+
 me.send_rc_control(0, 50, 0, 0)
 
 sleep(2)
 
 me.send_rc_control(0, 0, 0, 30)
+
+#sleep(2)
+
+me.flip_back()
 
 sleep(2)
 
