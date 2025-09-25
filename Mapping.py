bb2cdd97994d83cb1d9c36a975be12ed263c5a39
Snowlion <xnowlion@gmail.com>
2025-09-24 19:28:58 -0700

    Mapping 1

--- Mapping.py
+++ Mapping.py
@@ -1,41 +1,29 @@
 from djitellopy import tello
-
 import KeyPressModule as kp
-
 import numpy as np
-
 from time import sleep
 
 import cv2
-
 import math
 
-######## PARAMETERS ###########
+######## PARAMETERS ###########################
 
 fSpeed = 117 / 10  # Forward Speed in cm/s   (15cm/s)
-
 aSpeed = 360 / 10  # Angular Speed Degrees/s  (50d/s)
-
 interval = 0.25
 
-dInterval = fSpeed * interval
-
-aInterval = aSpeed * interval
+dInterval = fSpeed * interval # Distance Interval
+aInterval = aSpeed * interval # Angle Interval
 
 ###############################################
 
 x, y = 500, 500
-
-a = 0
-
-yaw = 0
+a = 0 # yaw angle
+yaw = 0 # yaw
 
 kp.init()
-
 me = tello.Tello()
-
 me.connect()
-
 print(me.get_battery())
 
 points = [(0, 0), (0, 0)]
@@ -44,9 +32,8 @@ points = [(0, 0), (0, 0)]
 def getKeyboardInput():
     lr, fb, ud, yv = 0, 0, 0, 0
 
-    speed = 15
-
-    aspeed = 50
+    speed = 15  # cm/s
+    aspeed = 50 # degrees/s
 
     global x, y, yaw, a
 
@@ -54,34 +41,26 @@ def getKeyboardInput():
 
     if kp.getKey("LEFT"):
 
-        lr = -speed
-
-        d = dInterval
-
-        a = -180
+        lr = -speed     # Left and Right Speed
+        d = dInterval   # Distance
+        a = -180        # Angle
 
     elif kp.getKey("RIGHT"):
 
         lr = speed
-
         d = -dInterval
-
         a = 180
 
     if kp.getKey("UP"):
 
-        fb = speed
-
+        fb = speed # Forward and Backward
         d = dInterval
-
         a = 270
 
     elif kp.getKey("DOWN"):
 
-        fb = -speed
-
+        fb = -speed # Forward and Backward
         d = -dInterval
-
         a = -90
 
     if kp.getKey("w"):
@@ -95,13 +74,11 @@ def getKeyboardInput():
     if kp.getKey("a"):
 
         yv = -aspeed
-
         yaw -= aInterval
 
     elif kp.getKey("d"):
 
         yv = aspeed
-
         yaw += aInterval
 
     if kp.getKey("q"): me.land(); sleep(3)
@@ -110,20 +87,18 @@ def getKeyboardInput():
 
     sleep(interval)
 
-    a += yaw
-
-    x += int(d * math.cos(math.radians(a)))
-
-    y += int(d * math.sin(math.radians(a)))
+    a += yaw    # Total Yaw
+    x += int(d * math.cos(math.radians(a))) # X coordinates
+    y += int(d * math.sin(math.radians(a))) # Y coordinates
 
     return [lr, fb, ud, yv, x, y]
 
 
 def drawPoints(img, points):
     for point in points:
-        cv2.circle(img, point, 5, (0, 0, 255), cv2.FILLED)
+        cv2.circle(img, point, 5, (0, 0, 255), cv2.FILLED)  # Previous Points in Red (trace)
 
-    cv2.circle(img, points[-1], 8, (0, 255, 0), cv2.FILLED)
+    cv2.circle(img, points[-1], 8, (0, 255, 0), cv2.FILLED) # Current Point
 
     cv2.putText(img, f'({(points[-1][0] - 500) / 100},{(points[-1][1] - 500) / 100})m',
 
@@ -135,7 +110,6 @@ def drawPoints(img, points):
 while True:
 
     vals = getKeyboardInput()
-
     me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
 
     img = np.zeros((1000, 1000, 3), np.uint8)
@@ -144,7 +118,5 @@ while True:
         points.append((vals[4], vals[5]))
 
     drawPoints(img, points)
-
     cv2.imshow("Output", img)
-
     cv2.waitKey(1)
